a21ead5fd85a51daab7773b5a784dadf42c54b55
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-29 12:17:15 +0300

    Debug-0x01-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+export default async function handleProfileSignup() {
+  return Promise.allSettled([uploadPhoto(), createUser()])
     .then(([p1, p2]) => console.log(p1.body, p2.firstName, p2.lastName))
     .catch(() => console.log('Signup system offline'));
 }
