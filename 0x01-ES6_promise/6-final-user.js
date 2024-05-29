233a21ceb888ab6a7fd4ad134dba982f129a3d85
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-29 16:55:02 +0300

    Debug-0x03-6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.value || res.reason,
+    })));
 }
