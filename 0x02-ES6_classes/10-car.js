16758a6fc82cb6b0721f0765fe0bddd02d9ccab3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-31 01:43:34 +0300

    Debug 0x01-10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,8 +1,8 @@
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
   get brand() { return (this._brand); }
@@ -10,6 +10,21 @@ export default class Car {
   get motor() { return (this._motor); }
 
   get color() { return (this._color); }
+
+  set brand(value) {
+    // if (typeof value !== 'string') throw new TypeError('Brand must be a string');
+    this._brand = value;
+  }
+
+  set motor(value) {
+    // if (typeof value !== 'string') throw new TypeError('Motor must be a string');
+    this._motor = value;
+  }
+
+  set color(value) {
+    // if (typeof value !== 'string') throw new TypeError('Color must be a string');
+    this._color = value;
+  }
 
   cloneCar() {
     if (this.constructor.name !== 'Car') return (new Car());
