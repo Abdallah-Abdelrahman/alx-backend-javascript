cccdf9a6d664608caf9f421ed76a2c6aa605aa34
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-31 01:39:09 +0300

    Add class `EVCar`

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,8 +1,8 @@
 export default class Car {
   constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   get brand() { return (this._brand); }
@@ -11,22 +11,8 @@ export default class Car {
 
   get color() { return (this._color); }
 
-  set brand(value) {
-    // if (typeof value !== 'string') throw new TypeError('Brand must be a string');
-    this._brand = value;
-  }
-
-  set motor(value) {
-    // if (typeof value !== 'string') throw new TypeError('Motor must be a string');
-    this._motor = value;
-  }
-
-  set color(value) {
-    // if (typeof value !== 'string') throw new TypeError('Color must be a string');
-    this._color = value;
-  }
-
   cloneCar() {
+    if (this.constructor.name !== 'Car') return (new Car());
     return (new this.constructor());
   }
 }
