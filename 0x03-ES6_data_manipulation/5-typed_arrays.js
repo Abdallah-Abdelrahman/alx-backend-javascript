8216e1b2a6164caa821a36d93bc3994991257e63
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-03 14:51:40 +0300

    Create typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+/**
+ * returns a new ArrayBuffer with an Int8 value at a specific position
+ * @param {number} length
+ * @param {number} position
+ * @param {number} value
+ *
+ * @returns ArrayBuffer
+ */
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  if (position >= length) throw new Error('Position outside range');
+  int8Array[position] = value;
+
+  return (buffer);
+}
