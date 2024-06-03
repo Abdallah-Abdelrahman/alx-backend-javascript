5a154625c34afa46a5c6f2d9312cdd8326f667b7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-03 17:55:16 +0300

    Return `Set` of a given array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * returns set of an array
+ * @param {Array<any>} array - list of any type
+ *
+ * @returns {Set} set of a given array
+ */
+export default function setFromArray(array) {
+  return (new Set(array));
+}
