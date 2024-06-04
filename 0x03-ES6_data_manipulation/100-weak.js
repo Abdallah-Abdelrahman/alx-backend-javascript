5c3c04a648d6771090d7201134e040fd5d876221
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-04 16:10:17 +0300

    Debug 0x02-100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,9 +6,9 @@ const weakMap = new WeakMap();
  * @param {string} endpoint.name
  */
 function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
   if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
 
 export { queryAPI, weakMap };
