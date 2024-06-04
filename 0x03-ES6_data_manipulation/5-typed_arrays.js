783c82d494d7f9d7b0d62e7cfac5c86af1329be4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-04 17:22:55 +0300

    Debug 0x04-100

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -7,10 +7,11 @@
  * @returns ArrayBuffer
  */
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  if (position >= length || value < -128 || value > 127) throw new Error('Position outside range');
-  int8Array[position] = value;
+  if (position >= length) throw new Error('Position outside range');
 
-  return (int8Array);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return (view);
 }
