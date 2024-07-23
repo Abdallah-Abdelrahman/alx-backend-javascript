fc4e74a9f7b7424ef6e4458c31e5add7275fac40
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-24 02:24:38 +0800

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+/**
+ * Attempt to read the database file synchronously
+ *
+ * @param {string} path - path to the database file
+ */
+function countStudents(path) {
+  try {
+    let buff = '';
+    // Check if the file exists
+    if (!fs.existsSync(path)) {
+      throw new Error('Cannot load the database');
+    }
+
+    // Read the file content synchronously
+    const fileContent = fs.readFileSync(path, 'utf8');
+
+    // Split the file content into lines
+    const lines = fileContent.trim().split('\n');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = {};
+    let totalStudents = 0;
+    let i = 0;
+
+    // Process each line except the header
+    for (const line of lines) {
+      // const line = lines[i].trim();
+      if (line !== '' && i > 0) {
+        const toks = line.split(',');
+        const field = toks[toks.length - 1];
+        const firstname = toks[0];
+
+        if (!students[field]) {
+          students[field] = { count: 0, list: [] };
+        }
+        students[field].count += 1;
+        students[field].list.push(firstname);
+        totalStudents += 1;
+      }
+      i += 1;
+    }
+
+    // Log the results
+    buff = `Number of students: ${totalStudents}\n`;
+    for (const [field, { count, list }] of Object.entries(students)) {
+      buff += `Number of students in ${field}: ${count}. List: ${list.join(', ')}\n`;
+    }
+    process.stdout.write(buff);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = countStudents;
