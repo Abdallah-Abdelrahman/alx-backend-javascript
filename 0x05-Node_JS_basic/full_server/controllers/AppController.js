e059d38e86c132eff6873c3529ab4a86e19342d1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-24 11:12:13 +0800

    Debug 0x02-8

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,12 +1,15 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-};
+/**
+ * Controller for handling requests to the homepage.
+ */
+class AppController {
+  /**
+   * Sends a greeting message.
+   * @param {Object} request - The request object.
+   * @param {Object} response - The response object.
+   */
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
