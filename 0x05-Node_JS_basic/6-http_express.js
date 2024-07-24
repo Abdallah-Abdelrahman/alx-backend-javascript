0d421ccc2614698d4efe4275e41c64b22912b1f0
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-24 10:49:14 +0800

    Create server using `Expressjs`

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+/**
+ * @module Express Server
+ *
+ * @returns {express.Express} The Express server.
+ */
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
